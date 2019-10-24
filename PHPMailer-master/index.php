008218d9b44cad91eb4f1c9a1d331606861b5b5d
viduranginawandana <47681119+viduranginawandana@users.noreply.github.com>
2019-10-24 12:53:38 +0530

    Add files via upload

---
+++ PHPMailer-master/index.php
@@ -0,0 +1,30 @@
+<?php
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'ashinashanuka@gmail.com';                 // SMTP username
+$mail->Password = '1234';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+$mail->SMTPDebug = 3;
+$mail->setFrom('ashinashanuka@gmail.com', 'AshinaShanuka');
+$mail->addAddress('lakshanvengetesh1234@gmail.com ', 'Laskshan');     // Add a recipient
+// $mail->addAddress('ellen@example.com');               // Name is optional
+// $mail->addReplyTo('info@example.com', 'Information');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Email From PHPMailer';
+$mail->Body    = 'Hello brother how are you this email is send form phpmailer library its very easy!</b>';
+$mail->AltBody = 'Hello brother how are you this email is send form phpmailer library its very easy!</';
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
